486e66be47e1a6f67a802848dafcd80315afd632
Shanek524 <31973478+Shanek524@users.noreply.github.com>
2019-02-24 18:30:08 -0500

    0.0.1

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const electron = require('electron')
+const path = require('path')
+const BrowserWindow = electron.remote.BrowserWindow
+
+const playBtn = document.getElementById('playBtn')
+
+playBtn.addEventListener('click', function(event){
+    const modalPath = path.join('file://', __dirname, 'game.html')
+    let win = new BrowserWindow({autoHideMenuBar: true, fullscreen: true})
+    win.loadURL(modalPath)
+    win.show()
+})
+
