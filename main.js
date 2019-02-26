5b1d21525ef22a5fa1b5fcd1bfd23e777604ec78
Shanek524 <31973478+Shanek524@users.noreply.github.com>
2019-02-26 14:36:06 -0500

    0.0.1

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createWindow () {
   win = new BrowserWindow({ width: 800, height: 600})
 
   // and load the index.html of the app.
-  win.loadFile('src/index.html')
+  win.loadFile('app/src/index.html')
 
   // Open the DevTools.
   // win.webContents.openDevTools()
